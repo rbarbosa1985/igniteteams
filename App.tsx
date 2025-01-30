e29574dcb88919adbe479edc34b0b2735a034752
Roberto Barbosa <betoanselmo@gmail.com>
2025-01-30 20:57:35 -0300

    ShowVerticalScroll

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { Loading } from "@components/Loading";
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
+import { Groups } from "@screens/Groups";
 import { NewGroup } from "@screens/NewGroup";
 import { Players } from "@screens/Players";
 import { StatusBar } from "react-native";
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,19 @@
-import { Header } from "@components/Header";
-import { Container } from "./styles";
-import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
 import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
 import { useState } from "react";
 import { FlatList } from "react-native";
-import { ListEmpty } from "@components/ListEmpty";
-import { Button } from "@components/Button";
+
+import { Container } from "./styles";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([
-    // "Rocket",
-    // "Galera do Ignite",
-    // "Grupo de amigos",
-    // "Família",
+    "Rocket",
+    "Galera do Ignite",
+    "Grupo de amigos",
+    "Família",
   ]);
 
   return (
@@ -27,6 +28,7 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+        showsVerticalScrollIndicator={false}
       />
       <Button title="Criar nova turma" />
     </Container>
